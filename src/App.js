fad5e5a8e22547ee1a4571c577940849fb30e66f
Vivek Kumar <vivek.network9525@gmail.com>
2022-05-02 20:01:50 +0530

    modifying App.js & Searchbar.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
-function App(props) {
+function App() {
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+  const apiKey = "007095548204eb9975bb507a5c3f2cdb";
+  const onCityChange = (city) => {
+    console.log(city);
+    axios.get(baseUrl + `q=${city}&appid=${apiKey}`);
+  };
   return (
     <React.Fragment>
       <Navbar></Navbar>
-      <Searchbar />
+      <Searchbar onSubmit={onCityChange} />
     </React.Fragment>
   );
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,41 +2,58 @@ import React, { useEffect, useState } from "react";
 import { getWeatherData } from "./WeatherApi";
 import styles from "./Searchbar.module.css";
 
-export default function Searchbar() {
-  const [weatherdata, setWeatherData] = useState(null);
-  const [city, setCity] = useState("patna");
+export default function Searchbar(props) {
+  // const [weatherdata, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
   // const [loading, setLoading] = useState(false);
 
-  const getData = async () => {
-    try {
-      const data = await getWeatherData(city);
-      setWeatherData(data);
-      console.log(data);
-    } catch (error) {
-      console.log(error.message);
-    }
+  // const getData = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const data = await getWeatherData(city);
+  //     setWeatherData(data);
+  //     // console.log(data);
+  //     setLoading(false);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //     setLoading(false);
+  //   }
+  // };
+
+  // const override
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  const onCitySubmit = () => {
+    // console.log(city);
+    props.onSubmit(city);
   };
-  useEffect(() => {
-    getData();
-  }, []);
   return (
     <React.Fragment>
       <div className={styles.searchbar_container}>
         <input
           className={styles.searchbar_input}
           type="text"
-          onChange={(event) => setCity(event.target.value)}
+          value={city}
+          // onChange={(event) => setCity(event.target.value)}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
           placeholder="Enter City Name..."
         />
-        <button
-          className={styles.searchbar_searchBtn}
-          onClick={() => {
-            getData();
-          }}
-        >
+        <button className={styles.searchbar_searchBtn} onClick={onCitySubmit}>
           Search
         </button>
-        {/* <Loading /> */}
+        {{
+          /* loading */
+        } ? (
+          <div className={styles.loader_container}>
+            <h4>Searching City name...</h4>
+          </div>
+        ) : (
+          " "
+        )}
       </div>
     </React.Fragment>
   );
